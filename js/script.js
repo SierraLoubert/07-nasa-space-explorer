d4c99ace644da070889fd74efac45d966bfb720d
Sierra L Loubert <Loube1sl@cmich.edu>
2026-07-10 23:34:09 +0000

    changed video text.

--- js/script.js
+++ js/script.js
@@ -187,7 +187,7 @@ async function getSpaceImages() {
                     <img src="${videoThumbnail}" alt="Thumbnail for ${photo.title}">
                   </button>`
                 : `<button type="button" class="media-button no-image">
-                    <span>🎥 Video Preview</span>
+                    <span>🎥 Video in Link Provided</span>
                   </button>`
                 : `<img 
                   src="${photo.url}" 
